65093b35b4157d041ebe147b99b7a438a9f5e604
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-10-27 19:19:58 -0300

    Update database.js

--- database.js
+++ database.js
@@ -4,9 +4,13 @@ import pkg from 'pg';
 const { Pool } = pkg;
 const app = express();
 
+
+// --------------------------------------------------------------------------------------
+// AUX
 app.use(cors());
 
-
+// --------------------------------------------------------------------------------------
+// CRENDENCIAL
 const pool = new Pool({
   user: 'postgres',
   host: 'gluttonously-bountiful-sloth.data-1.use1.tembo.io',
@@ -18,7 +22,10 @@ const pool = new Pool({
   },
 });
 
-async function executarConsulta() {
+// --------------------------------------------------------------------------------------
+// CARREGAR PRODUTOS
+
+async function listar_produtos() {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM tembo.tb_integracao');
@@ -32,10 +39,32 @@ async function executarConsulta() {
 }
 
 app.get('/dados', async (req, res) => {
-  const dadosArray = await executarConsulta();
+  const dadosArray = await listar_produtos();
   res.json(dadosArray);
 });
 
+// --------------------------------------------------------------------------------------
+// TABELA DE VENDAS
+
+async function listar_vendas() {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`SELECT * FROM tembo.tb_vendas WHERE "EMISSAO" >= '2024-01-09'`);
+    const dadosArray = result.rows;
+    client.release();
+    return dadosArray;
+  } catch (error) {
+    console.error('Erro ao conectar ou consultar o PostgreSQL:', error);
+    return [];
+  }
+}
+
+app.get('/vendas', async (req, res) => {
+  const dadosArray = await listar_vendas();
+  res.json(dadosArray);
+});
+
+// ----------------------------------------------------------------------------------------
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
