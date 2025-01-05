123aab8ba62d84417096390e00ca65d4cae3383d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-01-04 22:19:14 -0300

    Update database.js

--- database.js
+++ database.js
@@ -39,7 +39,47 @@ app.use(cors(corsOptions));
 async function tabela_integracao() {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM tembo.tb_integracao');
+    const result = await client.query(
+      
+      `WITH estoque_calculado AS ( 
+        SELECT 
+            e."SKU",
+            SUM(CASE 
+                    WHEN e."TIPO" = 'E' THEN e."QTD"
+                    WHEN e."TIPO" = 'S' THEN -e."QTD"
+                    ELSE 0
+                END) AS "ESTOQUE_TOTAL"
+        FROM 
+            tembo.tb_mov_estoque AS e
+        GROUP BY 
+            e."SKU"
+    )
+    SELECT 
+        cp."PARENT",
+      cp."DESCRICAO_PARENT" AS "DESCRICAO",
+      cp."CATEGORIA",
+      cp."VR_UNIT" AS "PRECO_DE",
+      ROUND(p."VR_UNIT" - (p."VR_UNIT" * 0.10), 2) AS "PRECO_POR",
+      p."ATIVO",
+      COALESCE(ec."ESTOQUE_TOTAL", 0) AS "ESTOQUE_VENDA",
+      pi."HOME",
+        cp."IMAGEM",
+      p."SKU",
+        p."VARIACAO"
+       
+    FROM 
+        tembo.tb_produto AS p
+    JOIN 
+        tembo.tb_produto_parent AS cp
+        ON p."PARENT" = cp."PARENT"
+    JOIN 
+        tembo.tb_integracao AS pi 
+        ON p."PARENT" = pi."PARENT"
+    LEFT JOIN 
+        estoque_calculado AS ec
+        ON p."SKU" = ec."SKU";`
+      
+      );
     const dadosArray = result.rows;
     client.release();
     return dadosArray;
