10153e25d55f2b2a3d23cd7fdca680ed62af85f5
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-03-10 12:31:36 -0300

    Update database.js

--- database.js
+++ database.js
@@ -149,31 +149,31 @@ async function select_powerbi() {
     const client = await pool.connect();
     const query = `
 
-            SELECT
-            v."PEDIDO",
-            v."SKU_CLIENTE",
-            v."EMISSAO",
-            v."PARENT",
-            p."CATEGORIA",
-            p."DESCRICAO",
-            v."QTD",
-            v."VR_UNIT",
-            v."STATUS",
-            c."CLIENTE",
-            c."CIDADE",
-            c."UF"
-        FROM tembo.tb_venda AS v
-        LEFT JOIN (
-            SELECT DISTINCT ON ("PARENT") 
-                "PARENT",
-                "DESCRICAO",
-                "CATEGORIA"
-            FROM tembo.tb_produto
-            ORDER BY "PARENT"
-        ) AS p ON v."PARENT" = p."PARENT"
-        LEFT JOIN tembo.tb_cliente AS c ON v."SKU_CLIENTE" = c."SKU_CLIENTE"
-        WHERE v."EMISSAO" >= '2024-01-01';
-
+    SELECT
+      v."PEDIDO",
+      v."SKU_CLIENTE",
+      v."EMISSAO",
+      TO_CHAR(v."EMISSAO", 'MM') AS mes,  -- Obtém o mês no formato "01", "02", etc.
+      v."PARENT",
+      p."CATEGORIA",
+      p."DESCRICAO",
+      v."QTD",
+      v."VR_UNIT",
+      v."STATUS",
+      c."CLIENTE",
+      c."CIDADE",
+      c."UF"
+      FROM tembo.tb_venda AS v
+      LEFT JOIN (
+          SELECT DISTINCT ON ("PARENT") 
+              "PARENT",
+              "DESCRICAO",
+              "CATEGORIA"
+          FROM tembo.tb_produto
+          ORDER BY "PARENT"
+      ) AS p ON v."PARENT" = p."PARENT"
+      LEFT JOIN tembo.tb_cliente AS c ON v."SKU_CLIENTE" = c."SKU_CLIENTE"
+      WHERE v."EMISSAO" >= '2024-01-01';
 
     `;
     const result = await client.query(query);
